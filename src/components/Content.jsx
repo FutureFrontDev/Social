dd1efcff0b6b4212f65ee5b06ba541d530ca19bb
FutureFrontDev <74024058+FutureFrontDev@users.noreply.github.com>
2020-12-05 01:47:34 +0200

    added all my project to git

---
+++ src/components/Content.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" />
+      </div>
+      <div>ava+desc</div>
+      <div>
+        My post
+        <div>New posts</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+          <div>post3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;

72c07e73d5492300bcb48b3533603608644356f8
Sergey <sergsmile29@gmail.com>
2021-03-16 23:48:20 +0200

    router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Nav from "./components/Navigation/Nav";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <Nav/>
+
+            <div className='app-wrapper-content '>
+                <Dialogs/>
+                {/*<Profile />*/}
+            </div>
+
+        </div>
+    );
 };
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import s from "./Profile.module.css";
 import Post from "./MyPosts/Post/Post.jsx";
+
 const Profile = () => {
-  return (
-    <div className={s.Profile}>
-        <div> <textarea ></textarea>
-            <button>Add</button></div>
+    return (
+        <div>
+        <div><textarea></textarea>
+            <button>Add</button>
+        </div>
 
-     <Post massage='Hi'/>
-     <Post message='Hello'/>
+        <Post massage='Hi' likes='23'/>
+        <Post massage='Hello' likes='2'/>
 
 
-    </div>
-  );
+</div>
+);
 };
 
 export default Profile;
